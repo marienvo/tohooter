002f57d95cc82fe04c3092fe5712d716f9eb5e9b
Marien van Overbeek <git@marienvanoverbeek.nl>
2024-03-07 16:37:18 +0100

    Add icon and app name

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 import sys
 from pathlib import Path
 
-from PySide6.QtGui import QGuiApplication
+from PySide6.QtGui import QGuiApplication, QIcon
 from PySide6.QtQml import QQmlApplicationEngine
 
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
+
+    # Set the application icon
+    app.setWindowIcon(QIcon('icon.png'))
+    app.setApplicationName("qTrace")
+
+
     engine = QQmlApplicationEngine()
     qml_file = Path(__file__).resolve().parent / "main.qml"
     engine.load(qml_file)
