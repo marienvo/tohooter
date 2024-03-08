0658f63a7052c1396f5192203af382cc5353716d
Marien van Overbeek <git@marienvanoverbeek.nl>
2024-03-08 13:57:03 +0100

    Cleaning things up

--- lib/app_loader.py
+++ lib/app_loader.py
@@ -2,12 +2,22 @@ import sys
 from pathlib import Path
 
 from PySide6.QtCore import QTimer
+from PySide6.QtGui import  QIcon
+from PySide6.QtQml import QQmlApplicationEngine
 from lib.settings_manager import load_settings, save_settings
 
-def app_loader(engine, splash, app_name, organization_name):
+# Todo: save and load settings from ~/Notes/todo.settings
+
+def app_loader(app, splash, app_name, organization_name):
+    app.setWindowIcon(QIcon('icon.png'))
+    app.setApplicationName(app_name)
+    app.setOrganizationName(organization_name)
+    app.setOrganizationDomain("marienvanoverbeek.nl")
+
+    engine = QQmlApplicationEngine()
     pos, size = load_settings(app_name, organization_name)
 
-    def internal_load_and_hide():
+    def load_and_hide():
         qml_file = Path(__file__).resolve().parent / "../main.qml"
         engine.load(qml_file)
         if not engine.rootObjects():
@@ -24,4 +34,4 @@ def app_loader(engine, splash, app_name, organization_name):
         window.closing.connect(lambda: save_settings(app_name, organization_name, window))
         splash.hide()
 
-    QTimer.singleShot(1000, internal_load_and_hide)
+    QTimer.singleShot(1000, load_and_hide)
--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QSplashScreen
-from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtGui import QPixmap
 from PySide6.QtCore import Qt
-from PySide6.QtQml import QQmlApplicationEngine
 
 from lib.app_loader import app_loader
-
-
-# Todo: save and load settings from ~/Notes/todo.settings
 
 def main():
     app = QApplication(sys.argv)
     splash = QSplashScreen(QPixmap('splash.png'), Qt.WindowStaysOnTopHint)
     splash.show()
-    app.setWindowIcon(QIcon('icon.png'))
-    app.setApplicationName("TraceTab")
-    app.setOrganizationName("marienvo")
-    app.setOrganizationDomain("marienvanoverbeek.nl")
-
-    engine = QQmlApplicationEngine()
-
-    # Call the load_and_hide_splash function
-    app_loader(engine, splash, "TraceTab", "marienvo")
-
+    app_loader(app, splash, "TraceTab", "marienvo")
     sys.exit(app.exec())
 
 if __name__ == "__main__":
