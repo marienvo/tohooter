4fc08863520ff81510c1881ade6a7b09c799d5ca
Marien van Overbeek <git@marienvanoverbeek.nl>
2024-03-09 13:02:31 +0100

    Enter enter

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication
 from lib.app_loader import app_loader
 from lib.splash import create_splash
 
+
 def main():
     app = QApplication(sys.argv)
     splash = create_splash(
@@ -16,6 +17,7 @@ def main():
     # Load the main application
     app_loader(app, splash, "Tohooter", "marienvo")
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
